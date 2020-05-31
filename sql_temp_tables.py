98a7939049dfdf9a7b58c334b2a1a95ceb95500c
ryan <ryanwaszak@gmail.com>
2020-05-31 08:58:56 -0500

    add first sql exercise

---
+++ sql_temp_tables.py
@@ -0,0 +1,64 @@
+list = [
+  ["Foster City", 50 ],
+  ["Fresno Armstrong Ave", 5 ],
+  ["Livermore", 20 ],
+  ["Martinez", 60 ],
+  ["Milpitas", 45 ],
+  ["Parker", 20 ],
+  ["Petaluma", 75 ],
+  ["Portland", 15 ],
+  ["Reno Capital Blvd", 5 ],
+  ["Sacramento", 40 ],
+  ["Salt Lake City WH", 5 ],
+  ["Seattle-Temp", 5 ],
+  ["Stockton", 10 ],
+  ["Albuquerque", 5 ],
+  ["Atwater Village", 10 ],
+  ["Austin WH", 10 ],
+  ["Dallas", 10 ],
+  ["Hawthorne", 15 ],
+  ["Inland Empire", 15 ],
+  ["Irvine", 25 ],
+  ["Phoenix Pinnacle Peak", 15 ],
+  ["San Diego", 25 ],
+  ["San Fernando Valley", 40 ],
+  ["Santa Maria", 15 ],
+  ["Sugarland", 10 ],
+  ["Vista", 15 ],
+  ["West Las Vegas", 10 ],
+  ["Beltsville", 35 ],
+  ["Bethpage", 10 ],
+  ["Burlington", 5 ],
+  ["Chicago X", 5 ],
+  ["Fort Lauderdale", 10 ],
+  ["Hartford", 10 ],
+  ["Marlborough", 10 ],
+  ["New Windsor", 5 ],
+  ["Norristown", 5 ],
+  ["Orlando", 20 ],
+  ["Pine Brook", 5 ],
+  ["Pittsburgh", 5 ],
+  ["Raynham", 15 ],
+  ["Seaford", 5 ],
+  ["Tampa", 20 ],
+  ["Wilmington Ballardva",10],
+]
+
+temp_table = 1
+
+for entry in list:
+  print(f"--{entry}")
+  print(f"IF OBJECT_ID('tempdb..#{temp_table}') IS NOT NULL BEGIN DROP TABLE #{temp_table} END")
+  print(f"select top {entry[1]}*")
+  print(f"INTO #{temp_table}")
+  print("FROM #ALLJOBS")
+  print(f"WHERE office in('{entry[0]}')")
+  print("")
+  print("")
+  temp_table += 1
+
+
+  # USEFUL RESOURCES
+  # iterate through array: https://snakify.org/en/lessons/two_dimensional_lists_arrays/
+  # string interpolation: https://realpython.com/python-string-formatting/
+  
